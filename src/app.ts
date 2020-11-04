b349ef7f26f9eaa91c2fc1fa476efeacca1e8185
thm2320 <70251202+thm2320@users.noreply.github.com>
2020-11-04 14:37:48 +0800

    restructure folder; add healthCheck and new route for tasks

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 
+import tasksRoutes from './routes/tasks';
+
 const app = express();
+const PORT = 8000;
 
-app.listen(3000);
 
-console.log('app started in port 3000')
+app.use('/tasks',tasksRoutes);
+
+app.get('/healthCheck', (req, res) => res.send('Server is running'))
+
+app.listen(PORT);
+
+console.log(`Server is running in port ${PORT}`)
